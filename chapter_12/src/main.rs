831b1c2d2351ddd40f608ee3cc6695e78bba24f2
Sindbad-Walter <sindbad.walter@gmail.com>
2022-11-04 01:08:54 -0400

    12.1/2/3 done

--- chapter_12/src/main.rs
+++ chapter_12/src/main.rs
@@ -1,3 +1,14 @@
+use chapter_12::Config;
+use std::{env, process};
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing argument : {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = chapter_12::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
