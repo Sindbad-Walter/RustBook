d42bd5f22c7a5af81b57c130dc7c67197d0f2cc8
Sindbad-Walter <sindbad.walter@gmail.com>
2023-05-08 12:56:29 +0200

    Tried stuff

--- chapter_07/src/garden/vegetables.rs
+++ chapter_07/src/garden/vegetables.rs
@@ -1,2 +1,18 @@
 #[derive(Debug)]
 pub struct Asparagus {}
+#[derive(Debug)]
+pub struct Zuchini {
+    size: u32,
+    looks: Looks,
+}
+
+#[derive(Debug)]
+pub enum Looks {
+    Bad,
+    Average,
+    Good,
+}
+
+pub fn zuchini_maker(size: u32, looks: Looks) -> Zuchini {
+    Zuchini { size, looks }
+}
--- chapter_07/src/main.rs
+++ chapter_07/src/main.rs
@@ -1,8 +1,10 @@
-use crate::garden::vegetables::Asparagus;
+use crate::garden::vegetables;
 
 pub mod garden;
 
 fn main() {
-    let plant = Asparagus {};
+    let plant = vegetables::Asparagus {};
     println!("I'm growing {:?}!", plant);
+    let plant2 = vegetables::zuchini_maker(12, vegetables::Looks::Good);
+    println!("{:?}", plant2);
 }
