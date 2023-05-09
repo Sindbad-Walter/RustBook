6fc37242e67442c780c3cc5350935a2f6c46b52b
Sindbad-Walter <sindbad.walter@gmail.com>
2023-05-09 10:49:53 +0200

    Replaced loops by iterators for readablility and performance0

--- chapter_12/src/main.rs
+++ chapter_12/src/main.rs
@@ -1,8 +1,7 @@
 use chapter_12::Config;
 use std::{env, process};
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing argument : {err}");
         process::exit(1);
     });
